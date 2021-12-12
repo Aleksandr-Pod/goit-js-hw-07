65e20fb64694999eb72ead42875e5f783df26c8d
Aleksandr-Pod <stdfire@gmail.com>
2021-12-12 14:41:39 +0200

    part 1 and 2 for inspection

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -4,51 +4,45 @@ console.log(galleryItems);
 
 const gallery = galleryItems.map(({ preview, original, description }) => {
     return `<div class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img class="gallery__image" src="${preview}" alt="${description}"
-        data-source=${original}/>
-    </a>
+        <a class="gallery__link" href=${original}>
+            <img
+                class="gallery__image"
+                src=${preview}
+                alt=${description}
+                data-source=${original}
+            />
+        </a>
     </div>`
-    
-    // const gItem = document.createElement("a");
-    // gItem.className = "gallery__link";
-    // gItem.href = original;
-    // const image = document.createElement("img")
-    // image.src = preview;
-    // image.alt = description;
-    // image.dataset.source = original;
-    // image.className = "gallery__image";
-
-    // gItem.appendChild(image);
-    // return (gItem);
 }).join("");
 
 const galleryRef = document.querySelector(".gallery");
 galleryRef.insertAdjacentHTML("afterbegin", gallery);
-// galleryRef.append(...gallery); // join - надо удалить
 
 
 galleryRef.addEventListener('click', onClick);
 function onClick(evt) {
     evt.preventDefault();
+    
+    window.addEventListener('keydown', onKeydown);
+
     if (evt.target.nodeName !== "IMG") {
+        window.removeEventListener('keydown', onKeydown);
         return
     }
     const imageSrc = evt.target.dataset.source;
-    console.log(imageSrc);
     const modal = basicLightbox.create(`<img src="${imageSrc}"/>`);
     modal.show();
-    // window.addEventListener('keydown', onKeydown);
-}
+    
 
-// function onKeydown(evtKeydown) {
-//     console.log(evtKeydown);
-//     if (evtKeydown.code === "Escape") {
-//         console.log("exit modal");
-//         galleryRef.removeEventListener('keydown', onKeydown);
-//         modal.close(); // Не работает !!! - modal is not defined
-//     };
-// }
- 
+    function onKeydown(evtKeydown) {
+        console.log("evtKeydown");
+        if (evtKeydown.code === "Escape") {
+            console.log("exit modal");
+            galleryRef.removeEventListener('keydown', onKeydown);
+            modal.close();
+            return
+        };
+    }
+}
 
 
